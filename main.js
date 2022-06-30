407d3b891c4312f19851c168aa3566ab7e0855c9
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-06-30 18:52:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+an_empty_array = [];
+statuss = document.getElementById("status").innerHTML;
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(800, 720);
+canvas.position(500,160);
+}
+
+
+function draw() {
+if(statuss =! "") {
+
+}
+}
+function gotResults(errors , results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
